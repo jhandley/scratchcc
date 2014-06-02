4c33f6e59c2e3ab0dc784d23ca06e13ad3afd069
Frank Hunleth <fhunleth@troodon-software.com>
2014-06-01 20:04:32 -0400

    Add code template

--- test/scratchcc_test.exs
+++ test/scratchcc_test.exs
@@ -4,6 +4,7 @@ defmodule ScratchccTest do
   test "say script" do
     x = Scratchcc.doit([[136, 85, [["whenGreenFlag"], ["say:", "Hello!"]]]])
     IO.inspect x
+    File.write("test.c", x)
     assert 1 + 1 == 2
   end
 end
